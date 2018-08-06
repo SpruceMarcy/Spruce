cd951ae91653e9b3d421f521368588f9da4f6477
SpruceMarcy <mdr.brook@gmail.com>
2018-08-06 20:58:24 +0100

    Added the savegame class

---
+++ SpruceGame/SpruceGame/SaveGame.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;///MB: Imports dictionaries
+#pragma warning disable CS0618//MB: This disables the depreciated method warning
+
+namespace SpruceGame
+{
+    public class SaveGame
+    {
+        // - - - - Variables Global to this Save
+        readonly string SaveName;
+        byte[] Seed;
+        // - - - - - - - - - - - - - - - - - - -
+
+        public SaveGame(byte[] Seed) //sub new
+        {
+            this.Seed = Seed;
+        }
+
+        public void Update() //backend
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch) //frontend
+        {
+
+        }
+    }
+}
+#pragma warning restore CS0618
