50ae0be6abede802bafe0fc31cf7728d26fcccd4
SpruceMarcy <mdr.brook@gmail.com>
2019-08-04 22:09:44 +0100

    Player Abstracted

---
+++ SpruceGame/SpruceGame/Player.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpruceGame
+{
+    [Serializable]
+    class Player
+    {
+        public string textureKey; //MB: A key for the texture dictionary to retrieve the player texture
+        public Coord pos; //MB: The position of the player in the level. May want to move this to Level
+        float angle;
+        public Player()
+        {
+
+        }
+        public void Update(KeyboardState keyboardState, MouseState mouseState)
+        {
+            angle = (float)Math.Atan2(mouseState.Position.Y - 540, mouseState.Position.X - 960) + MathHelper.PiOver2;
+        }
+        public void Draw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, Dictionary<string, Texture2D> textureDict)
+        {
+            //MB: This is a bodge-together approach to drawing the player at an angle since the center of rotation is in the top left of a texture by default
+            Texture2D PlayerTexture = textureDict[textureKey];
+            Point SpritePosition = new Point(960, 540);
+            spriteBatch.Draw(PlayerTexture, new Rectangle(SpritePosition, new Point(PlayerTexture.Width, PlayerTexture.Height)), null, Color.Black, angle, new Vector2(PlayerTexture.Width / 2f, PlayerTexture.Height / 2f), SpriteEffects.None, 0);
+        }
+    }
+}
--- SpruceGame/SpruceGame/SaveGame.cs
+++ SpruceGame/SpruceGame/SaveGame.cs
@@ -15,16 +15,16 @@ namespace SpruceGame
         readonly string saveName;//MB: No use yet
         byte[] seed; //MB: The seed for all randomly determined elements
         Level loadedLevel; //MB: The labyrinth that is being played
-        string playerTextureKey; //MB: A key for the texture dictionary to retrieve the player texture
-        Coord playerPos; //MB: The position of the player in the level. May want to move this to Level
+        Player player;
         // - - - - - - - - - - - - - - - - - - -
 
         public SaveGame(byte[] seed, Dictionary<string, Texture2D> textureDict, string dataPackKey) //MB: on instanciation
         {
             this.seed = seed;
             loadedLevel = new Level(5,5,dataPackKey,seed,15); //MB: Create a new placeholder level
-            playerPos = new Coord(300, 300); //MB: Sets the player to (300,300) just as a placeholder
-            playerTextureKey = "Player";
+            player = new Player();
+            player.pos = new Coord(300, 300); //MB: Sets the player to (300,300) just as a placeholder
+            player.textureKey = "Player";
         }
 
         public void Update(KeyboardState keyboardState, MouseState mouseState) //MB: Game Logic (single frame)
@@ -32,28 +32,28 @@ namespace SpruceGame
             Coord movementVector = new Coord(0,0);//MB: This variable records where the player is moving next
             if (keyboardState.IsKeyDown(Keys.W))
             {
-                if (!IsSolid(playerPos + new Coord(0,-1)))
+                if (!IsSolid(player.pos + new Coord(0,-1)))
                 {
                     movementVector +=new Coord(0,-1); //MB: If "W" is pressed, move up (y values go up as you go down the screen)
                 }
             }
             if (keyboardState.IsKeyDown(Keys.S))
             {
-                if (!IsSolid(playerPos + new Coord(0, 1)))
+                if (!IsSolid(player.pos + new Coord(0, 1)))
                 {
                     movementVector += new Coord(0, 1);  //MB: If "S" is pressed, move down
                 }
             }
             if (keyboardState.IsKeyDown(Keys.A))
             {
-                if (!IsSolid(playerPos + new Coord(-1, 0))) //MB: If "A" is pressed, move left
+                if (!IsSolid(player.pos + new Coord(-1, 0))) //MB: If "A" is pressed, move left
                 {
                     movementVector += new Coord(-1, 0); //MB: If "A" is pressed, move left
                 }
             }
             if (keyboardState.IsKeyDown(Keys.D))
             {
-                if (!IsSolid(playerPos + new Coord(1,0))) //MB: If "D" is pressed, move right
+                if (!IsSolid(player.pos + new Coord(1,0))) //MB: If "D" is pressed, move right
                 {
                     movementVector += new Coord(1, 0); //MB: If "D" is pressed, move right
                 }
@@ -64,29 +64,23 @@ namespace SpruceGame
             }
             for (int i = 1; i <= 4; i++)
             {
-                if (!IsSolid(playerPos+movementVector*i))
+                if (!IsSolid(player.pos+movementVector*i))
                 {
-                    playerPos += movementVector;//MB: Moves to the next location if the new location is not in a wall or something
+                    player.pos += movementVector;//MB: Moves to the next location if the new location is not in a wall or something
                 }
                 else
                 {
                     break;
                 }
             }
-
-            loadedLevel.Update(mouseState, new Coord(960, 540) - playerPos);//MB: run the level logic
+            player.Update(keyboardState,mouseState);
+            loadedLevel.Update(mouseState, new Coord(960, 540) - player.pos);//MB: run the level logic
         }
 
         public void Draw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, Dictionary<string, Texture2D> textureDict, Dictionary<string, MapDataPack> dataPacks) //MB: frontend stuff
         {
-            loadedLevel.Draw(spriteBatch,new Coord(960,540)-playerPos,graphicsDevice,textureDict,dataPacks); //MB: Draw the level
-            {
-                //MB: This is a bodge-together approach to drawing the player at an angle since the center of rotation is in the top left of a texture by default
-                Texture2D PlayerTexture = textureDict[playerTextureKey];
-                float angle = (float)Math.Atan2(Mouse.GetState().Position.Y - 540, Mouse.GetState().Position.X - 960) + MathHelper.PiOver2;
-                Point SpritePosition = new Point(960,540);
-                spriteBatch.Draw(PlayerTexture, new Rectangle(SpritePosition, new Point(PlayerTexture.Width, PlayerTexture.Height)), null, Color.Black, angle, new Vector2(PlayerTexture.Width/2f,PlayerTexture.Height/2f), SpriteEffects.None, 0);
-            }
+            loadedLevel.Draw(spriteBatch,new Coord(960,540)-player.pos,graphicsDevice,textureDict,dataPacks); //MB: Draw the level
+            player.Draw(spriteBatch,graphicsDevice,textureDict);
         }
         /// <summary>
         /// Returns whether or not the specified position is occupied, preventing movement.
